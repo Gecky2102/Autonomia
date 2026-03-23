f2d5728d548b96d51cc757cabbbc8442211ad9bc
Gecky2102 <giacomomasiero.08@gmail.com>
2026-03-23 08:37:23 +0100

    Refactor SOSController to improve readability and maintainability

--- 160326/thread.py
+++ 160326/thread.py
@@ -3,6 +3,7 @@ from signal import pause
 import threading
 import time
 
+
 class SOSController:
 	
 	def __init__(self, pin_giallo=17, pin_rosso=27, btn_giallo=22, btn_rosso=23):
@@ -13,19 +14,20 @@ class SOSController:
 		self.pulsante_rosso = Button(btn_rosso, pull_up=True, bounce_time=0.05)
 
 		self.thread_sos = None
-		self.stop_event = threading.Event()  # ← aggiunto
+		self.stop_event = threading.Event() 
 
 		self.pulsante_giallo.when_pressed = self.start
 		self.pulsante_rosso.when_pressed = self.stop
 
 	def _sleep_interrompibile(self, secondi):
-		"""Aspetta X secondi, ma si ferma subito se arriva lo stop."""
+		
 		self.stop_event.wait(timeout=secondi)
 
 	def _sos_loop(self):
+		print("")
 		print("SOS - AIUTO!!!")
 		while not self.stop_event.is_set():
-			# Tre lampi corti (S)
+			
 			for _ in range(3):
 				if self.stop_event.is_set(): break
 				self.rosso.on()
@@ -33,7 +35,6 @@ class SOSController:
 				self.rosso.off()
 				self._sleep_interrompibile(1)
 
-			# Tre lampi lunghi (O)
 			for _ in range(3):
 				if self.stop_event.is_set(): break
 				self.rosso.on()
@@ -41,7 +42,6 @@ class SOSController:
 				self.rosso.off()
 				self._sleep_interrompibile(1)
 
-			# Tre lampi corti (S)
 			for _ in range(3):
 				if self.stop_event.is_set(): break
 				self.rosso.on()
@@ -49,13 +49,14 @@ class SOSController:
 				self.rosso.off()
 				self._sleep_interrompibile(1)
 
-		self.rosso.off()  # Assicura che il LED si spenga
+		self.rosso.off()
+		print("")  
 		print("Loop SOS terminato")
 
 	def start(self):
 		if not self.stop_event.is_set() and self.thread_sos and self.thread_sos.is_alive():
-			return  # Già in esecuzione
-		self.stop_event.clear()  # Resetta l'evento
+			return  
+		self.stop_event.clear()  
 		self.thread_sos = threading.Thread(target=self._sos_loop, daemon=True)
 		self.thread_sos.start()
 		print("Thread SOS avviato")
@@ -63,8 +64,8 @@ class SOSController:
 	def stop(self):
 		if not self.thread_sos or not self.thread_sos.is_alive():
 			return
-		self.stop_event.set()  # ← manda il segnale di stop
-		self.thread_sos.join() # ← aspetta che il thread finisca davvero
+		self.stop_event.set()  
+		self.thread_sos.join() 
 		print("Thread fermato")
 
 controller = SOSController()
