8f7f0c1a2daf69b5c91ac754a84c64999262fb97
Giacomo Masiero <90463906+Gecky2102@users.noreply.github.com>
2026-03-23 08:28:25 +0100

    Refactor SOSController to use stop_event for control
    Introduced a stop_event to manage thread execution and added a method for interruptible sleep. Updated SOS loop logic to utilize the stop_event for stopping the SOS signal.

--- 160326/thread.py
+++ 160326/thread.py
@@ -8,55 +8,63 @@ class SOSController:
 	def __init__(self, pin_giallo=17, pin_rosso=27, btn_giallo=22, btn_rosso=23):
 		
 		self.giallo = LED(pin_giallo)
-		
 		self.rosso = LED(pin_rosso)
 		self.pulsante_giallo = Button(btn_giallo, pull_up=True, bounce_time=0.05)
 		self.pulsante_rosso = Button(btn_rosso, pull_up=True, bounce_time=0.05)
 
 		self.thread_sos = None
-		self.sos_attivo = False
+		self.stop_event = threading.Event()  # ← aggiunto
 
-		
 		self.pulsante_giallo.when_pressed = self.start
 		self.pulsante_rosso.when_pressed = self.stop
 
+	def _sleep_interrompibile(self, secondi):
+		"""Aspetta X secondi, ma si ferma subito se arriva lo stop."""
+		self.stop_event.wait(timeout=secondi)
+
 	def _sos_loop(self):
 		print("SOS - AIUTO!!!")
-		while self.sos_attivo:
-			for i in range(3):
-				if not self.sos_attivo:
-					break
+		while not self.stop_event.is_set():
+			# Tre lampi corti (S)
+			for _ in range(3):
+				if self.stop_event.is_set(): break
 				self.rosso.on()
-				time.sleep(1)
+				self._sleep_interrompibile(1)
 				self.rosso.off()
-				time.sleep(1)
-			for i in range(3):
-				if not self.sos_attivo:
-					break
+				self._sleep_interrompibile(1)
+
+			# Tre lampi lunghi (O)
+			for _ in range(3):
+				if self.stop_event.is_set(): break
 				self.rosso.on()
-				time.sleep(2)
+				self._sleep_interrompibile(2)
 				self.rosso.off()
-				time.sleep(1)
-			for i in range(3):
-				if not self.sos_attivo:
-					break
+				self._sleep_interrompibile(1)
+
+			# Tre lampi corti (S)
+			for _ in range(3):
+				if self.stop_event.is_set(): break
 				self.rosso.on()
-				time.sleep(1)
+				self._sleep_interrompibile(1)
 				self.rosso.off()
-				time.sleep(1)
+				self._sleep_interrompibile(1)
+
+		self.rosso.off()  # Assicura che il LED si spenga
+		print("Loop SOS terminato")
 
 	def start(self):
-		if self.sos_attivo:
-			return
-		self.sos_attivo = True
+		if not self.stop_event.is_set() and self.thread_sos and self.thread_sos.is_alive():
+			return  # Già in esecuzione
+		self.stop_event.clear()  # Resetta l'evento
 		self.thread_sos = threading.Thread(target=self._sos_loop, daemon=True)
 		self.thread_sos.start()
-		print("Thread sos avviato")
+		print("Thread SOS avviato")
 
 	def stop(self):
-		if not self.sos_attivo:
+		if not self.thread_sos or not self.thread_sos.is_alive():
 			return
-		self.sos_attivo = False
+		self.stop_event.set()  # ← manda il segnale di stop
+		self.thread_sos.join() # ← aspetta che il thread finisca davvero
 		print("Thread fermato")
 
 controller = SOSController()
